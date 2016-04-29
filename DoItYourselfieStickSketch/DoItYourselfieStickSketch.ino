f5478872406162e8d534493c6c82918209d196f6
Clint Chilcott <clintchil@gmail.com>
2016-04-29 12:45:22 -0700

    add Arduino sketch and schematic

---
+++ DoItYourselfieStickSketch/DoItYourselfieStickSketch.ino
@@ -0,0 +1,26 @@
+/*
+  Do It Yourselfie Stick 
+  2016
+  
+  This code is in the public domain
+*/
+
+int previousButtonReading = HIGH;
+
+void setup() {
+  BeanHid.enable();
+
+  pinMode(0, INPUT_PULLUP);
+}
+
+void loop() {
+  int buttonReading = digitalRead(0);
+
+  // pullup inverts the logic
+  // HIGH = open, LOW = button pressed
+  if (buttonReading == LOW && previousButtonReading == HIGH) {
+    BeanHid.sendMediaControl(VOLUME_UP);
+  }
+
+  previousButtonReading = buttonReading;
+}
